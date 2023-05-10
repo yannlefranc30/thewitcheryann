efba4b00b030756c306aa94414887dfb496288cf
yann <yannlefranc330@gmail.com>
2023-05-10 08:12:28 +0200

    projet php the witcher

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+$thewitcher = date("28"); //si $thewitcher est inférieur à 14 alors have good day
+//si je possitionne 28 à la place de 14 alors good night
+if ($thewitcher < "20") {
+    echo "Have a good day!";
+} else {
+    echo "Have a good night!";
+}
+?>
+
+
